bf3c2f64c934a8931d96e111c63b88709cad7087
Goddreamwt <zm804890716@163.com>
2018-09-17 14:46:50 +0800

    完成首页

--- Handbook/miniprogram/pages/addHabit/addHabit.js
+++ Handbook/miniprogram/pages/addHabit/addHabit.js
@@ -1,45 +1,20 @@
-var animationUtil = require('./../../utils/animationUtil.js')
-
+// pages/addHabit/addHabit.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    logged: false,//是否登录
-    animationMiddleHeaderItem:{},//心跳动画
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    animationUtil.animationMiddleHeaderItem(this);
 
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              console.log(res);
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
   },
-  //添加习惯
-  addSubmit: function (event) {
-    wx.redirectTo({
-      url: '../addHabit/addHabit',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
